a4ec9b9541ed2be4b5edd59ee3ee93394ef0429b
Wout Schoovaerts <schoovaertswout@live.be>
2019-06-09 12:29:52 +0200

    StartupController ; TransformUtils

--- Assets/Scripts/Controllers/StartupController.cs
+++ Assets/Scripts/Controllers/StartupController.cs
@@ -1,9 +1,15 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Controllers
 {
+    // TODO: Needs to become a singleton component
     public class StartupController : MonoBehaviour
     {
-        
+        public void Start()
+        {
+            Random.InitState(DateTime.Now.Millisecond);
+        }
     }
 }
--- Assets/Scripts/Utils/TransformUtils.cs
+++ Assets/Scripts/Utils/TransformUtils.cs
@@ -1,7 +1,13 @@
+using UnityEngine;
+
 namespace Utils
 {
-    public class TransformUtils
+    public static class TransformUtils
     {
-        
+        public static float LookAt(Vector2 currentPosition, Vector2 lookAtPosition)
+        {
+            var angleInRad = Mathf.Atan2(lookAtPosition.y - currentPosition.y, lookAtPosition.x - currentPosition.x);
+            return angleInRad * Mathf.Rad2Deg;
+        }
     }
 }
